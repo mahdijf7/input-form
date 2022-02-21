73c0c317bdd7a57da8556368b2c3d4375076d0ce
mahdijf7 <mahdi.1998j@gmail.com>
2022-02-21 22:01:59 +0330

    add-form-validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,62 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameisValid, setEnteredNameIsvalid] = useState(false);
+  const [enteredNameTuch, setEnteredNameTuch] = useState(false);
+
+  const nameInputHandler = (e) => {
+    setEnteredName(e.target.value);
+    if (e.target.value !== '') {
+      setEnteredNameIsvalid(true);
+    }
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameTuch(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsvalid(false);
+    }
+  };
+
+  const addSubmitHandler = (e) => {
+    e.preventDefault();
+    setEnteredNameTuch(true);
+    if (enteredName.trim() === '') {
+      setEnteredNameIsvalid(false);
+      return;
+    }
+    setEnteredNameIsvalid(true);
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+
+    console.log(enteredValue);
+    setEnteredName('');
+  };
+  const inputIsInvalid = !enteredNameisValid && enteredNameTuch;
+
+  const nameInputClasses = inputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control ';
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={addSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {inputIsInvalid && <p className="error-text">form is empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button>Submit </button>
       </div>
     </form>
   );
